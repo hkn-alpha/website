6450fb7ac49c370edc2833b1811784b86ce6365d
Ryan Ziegler <rzig408@gmail.com>
2023-05-21 15:07:09 -0500

    emergency push

--- src/StudentServices.svelte
+++ src/StudentServices.svelte
@@ -2,6 +2,25 @@
   import Nav from "./components/Nav.svelte";
   import ReviewsCalendar from "./components/ReviewsCalendar.svelte";
   import { reviewSessions, crammingCarnival } from "./content/review_sessions";
+  import { tutors } from "./content/tutors";
+
+  function standardizeClassName(name) {
+    return name.toUpperCase().replaceAll(" ", "").replaceAll(",", "");
+  }
+
+  function getMatchingTutors(query) {
+    const requestedCourses = query.split(",").map(standardizeClassName);
+    return tutors.filter((tutor) => {
+      const standardizedTutorCourses = tutor.courses
+        .split(",")
+        .map(standardizeClassName);
+      let found = false;
+      requestedCourses.forEach((c) => {
+        found = found || standardizedTutorCourses.includes(c);
+      });
+      return found;
+    });
+  }
 </script>
 
 <!--
@@ -15,7 +34,9 @@
       Filler text about services that HKN offers. This should probably take up
       at least three lines. More filler text to make this more visually
       appealing. Here is even more filler text to make if even more appealing.
-      Wow, this website is finally taking shape.
+      Wow, this website is finally taking shape. <a href="#tutoring"
+        >Jump to tutors</a
+      >
     </p>
     {#each Object.keys(reviewSessions) as mtNum}
       {#if reviewSessions[mtNum].length > 0}
@@ -28,6 +49,17 @@
       <h1>Cramming Carnival</h1>
       <ReviewsCalendar sessions={crammingCarnival} />
     {/if}
+
+    <h1 id="tutoring">Find a Tutor</h1>
+    <p>
+      Need more personalized help? HKN offers free one-on-one peer tutoring.
+      Simply enter your class(es) below, and contact a tutor to arrange a time.
+    </p>
+    <input
+      class="tutor-search"
+      type="text"
+      placeholder="Search for courses (comma-separated)"
+    />
   </div>
 </div>
 
@@ -54,5 +86,29 @@
     font-size: 20px;
     color: white;
     margin-bottom: 60px;
+  }
+  a {
+    color: white !important;
+  }
+
+  .tutor-search {
+    width: 100%;
+    background-color: #0f2040;
+    border: none;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    padding-left: 2px;
+    padding-right: 2px;
+    color: white;
+    font-family: "Lora";
+    font-size: 18px;
+    border-bottom: 3px solid #546482;
+    /* margin-top: -120px; */
+    margin-bottom: 20px;
+  }
+
+  .tutor-search::placeholder {
+    color: #ddd;
+    font-family: "Lora";
   }
 </style>
---
+++ src/content/tutors.ts
@@ -0,0 +1,22 @@
+type Tutor = {
+  name: string;
+  email: string;
+  /**
+   * Comma-separated string of courses
+   */
+  courses: string;
+};
+
+export const tutors: Tutor[] = [
+  {
+    name: "Grant Mckechnie",
+    email: "granttm2@illinois.edu",
+    courses: "ECE 110, ECE 120",
+  },
+  {
+    name: "Alex Zhang",
+    email: "alexmz2@illinois.edu",
+    courses:
+      "ECE374, CS374, ECE 110, ECE 210, ECE313, ECE220, ECE 120, PHYS 211",
+  },
+];
