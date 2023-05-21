b1c8fea08c189072960a1dadd32082e168631155
Ryan Ziegler <rzig408@gmail.com>
2023-05-21 18:51:03 -0500

    tutor search

--- src/StudentServices.svelte
+++ src/StudentServices.svelte
@@ -2,7 +2,12 @@
   import Nav from "./components/Nav.svelte";
   import ReviewsCalendar from "./components/ReviewsCalendar.svelte";
   import { reviewSessions, crammingCarnival } from "./content/review_sessions";
-  import { tutors } from "./content/tutors";
+  import { tutors as unorderedTutors } from "./content/tutors";
+
+  const tutors = unorderedTutors
+    .map((x) => ({ v: x, r: Math.random() }))
+    .sort((x) => x.r)
+    .map((x) => x.v);
 
   function standardizeClassName(name) {
     return name.toUpperCase().replaceAll(" ", "").replaceAll(",", "");
@@ -21,6 +26,12 @@
       return found;
     });
   }
+
+  let query = "";
+
+  // console.log(query);
+
+  let matchingTutors = query == "" ? getMatchingTutors(query) : null;
 </script>
 
 <!--
@@ -51,15 +62,50 @@
     {/if}
 
     <h1 id="tutoring">Find a Tutor</h1>
-    <p>
+    <p class="instructions">
       Need more personalized help? HKN offers free one-on-one peer tutoring.
       Simply enter your class(es) below, and contact a tutor to arrange a time.
     </p>
     <input
       class="tutor-search"
       type="text"
-      placeholder="Search for courses (comma-separated)"
+      placeholder="Search for courses (comma-separated, e.x. ECE110, ECE120)"
+      bind:value={query}
     />
+    {#if getMatchingTutors(query).length > 0}
+      <div class="table-container">
+        <table>
+          <colgroup>
+            <col span="1" style="width: 30%;" />
+            <col span="1" style="width: 30%;" />
+            <col span="1" style="width: 40%;" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Courses offered</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each getMatchingTutors(query) as tutor}
+              <tr>
+                <td>{tutor.name}</td>
+                <td><a href={`mailto:${tutor.email}`}>{tutor.email}</a></td>
+                <td>{tutor.courses}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    {:else if query == ""}
+      <p class="needs-query">Start typing to see results...</p>
+    {:else}
+      <p class="no-results">
+        We couldn't find any tutors. Make sure your course names and numbers are
+        correct.
+      </p>
+    {/if}
   </div>
 </div>
 
@@ -110,5 +156,54 @@
   .tutor-search::placeholder {
     color: #ddd;
     font-family: "Lora";
+  }
+
+  table {
+    width: 100%;
+    color: white;
+    border-collapse: collapse;
+    margin-bottom: 60px;
+    min-width: 700px;
+  }
+
+  .table-container {
+    overflow-x: scroll;
+    scrollbar-width: none;
+  }
+
+  .table-container::-webkit-scrollbar {
+    display: none;
+  }
+
+  tr {
+    border-bottom: 2px solid white;
+    line-height: 50px;
+  }
+
+  thead tr {
+    border-bottom: 4px solid white;
+  }
+
+  tbody tr:nth-child(even) {
+    /** CSS 1-indexes children */
+    background-color: #2f3e59;
+  }
+
+  td {
+    text-align: center;
+  }
+
+  a {
+    color: white !important;
+  }
+
+  .instructions {
+    margin-bottom: 15px;
+  }
+
+  .needs-query,
+  .no-results {
+    margin-top: 5px;
+    text-align: center;
   }
 </style>
