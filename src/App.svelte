293bd564532dea21c7d07aa53ee451629a798fe7
Ryan Ziegler <rzig408@gmail.com>
2023-06-17 15:07:00 -0500

    add 404 page

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
   import { Router, Route } from "svelte-routing";
   import Home from "./pages/Home.svelte";
   import StudentServices from "./pages/StudentServices.svelte";
+  import FourOhFour from "./pages/FourOhFour.svelte";
 </script>
 
 <Router>
   <Route component={Home} path="/" />
   <Route component={StudentServices} path="/services" />
+  <Route component={FourOhFour} path="/*" />
 </Router>
