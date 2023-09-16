bbdbe020ca48c9cae4a310f9168b4a4ecc3906fb
Ryan Ziegler <rzig408@gmail.com>
2023-09-16 11:24:25 -0500

    add slides and 374A

--- src/content/StudentServices/review_sessions.ts
+++ src/content/StudentServices/review_sessions.ts
@@ -29,6 +29,8 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]> = {
       course: "ECE 210",
       time: "9/16, 12:30-3:00PM",
       location: "ECEB 1013",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1CXofOoxZtO_cR1BebHQkYRe13f3pgFGrI8qTSA3OKug/edit?usp=sharing",
     },
     {
       course: "ECE 329",
@@ -39,36 +41,55 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]> = {
       course: "ECE 120",
       time: "9/16, 3:00-5:30PM",
       location: "ECEB 1015",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1I4ZS-_0Etu7jwnx0D7A4Scd7oEGkFKFMThA1Q-XDR48/edit?usp=sharing",
     },
     {
       course: "ECE 350",
       time: "9/16, 3:00-5:30PM",
       location: "ECEB 3015",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1ZE0piNmhcMcQdak_Kk_NHYDkpmR82QnQDZGtrPL89wA/edit?usp=sharing",
     },
     {
       course: "ECE 374B",
       time: "9/17, 12:30-3:00PM",
       location: "ECEB 1002",
+      slidesLink:
+        "https://drive.google.com/file/d/17L-drxEQNKRLHoSRvSBL7QIub20A1ADJ/view?usp=sharing",
     },
     {
       course: "ECE 220",
       time: "9/17, 12:30-3:00PM",
       location: "ECEB 1015",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1cpcY2Ew17Ohvo0O3EfwxhTan90mxuxk3ixhEac0M4wg/edit?usp=sharing",
     },
     {
       course: "ECE 342",
       time: "9/17, 12:30-3:00PM",
       location: "ECEB 3017",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1L9h0rWMtx8yyRTc59ia8tQt4R-iXdfYbDpD09YZsFok/edit?usp=sharing",
     },
     {
       course: "ECE 110",
       time: "9/17, 3:00-5:30PM",
       location: "ECEB 1015",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1Oh-aqrL1W4NiW9eirWWPyRiRd8GkcS8DU-xe6mHMP-4/edit?usp=sharing",
     },
     {
       course: "ECE 411",
       time: "9/17, 3:00-5:30PM",
       location: "ECEB 3017",
+      slidesLink:
+        "https://docs.google.com/presentation/d/1j_Zn5haeTgr32OCv5dbMbQ1pwwj98LcdMG4gOxf2Gy0/edit?usp=sharing",
+    },
+    {
+      course: "CS 374A",
+      time: "9/24 3:00-5:30PM",
+      location: "ECEB 1002",
     },
   ],
   2: [],
