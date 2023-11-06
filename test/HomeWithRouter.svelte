6e6785084eb035cd9f9536bf63ce692f6fa709a4
Ryan Ziegler <rzig408@gmail.com>
2023-11-06 14:43:52 -0600

    initial try at tests for github

---
+++ test/HomeWithRouter.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import Home from "../src/pages/Home.svelte";
+</script>
+
+<Router>
+  <Route component={Home} path="/" />
+</Router>
---
+++ test/ServicesWithRouter.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import StudentServices from "../src/pages/StudentServices.svelte";
+</script>
+
+<Router>
+  <Route component={StudentServices} path="/" />
+</Router>
