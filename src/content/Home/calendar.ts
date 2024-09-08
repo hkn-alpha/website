70203d3d129fd06d2c1edc750f217db94f1b24ef
unknown <kyle11067@gmail.com>
2024-09-08 18:13:00 -0500

    Other events don't work

--- src/content/Home/calendar.ts
+++ src/content/Home/calendar.ts
@@ -327,12 +327,12 @@ const events: Event[] = [
 // ...[
 //   ,new Date(2024, 8, 9)
 // ].map(milleniumTalk),
-...[
-  ,new Date(2024,8,10)
-].map(BMcDTalk),
-...[
-  ,new Date(2024, 8, 11)
-].map(halliburtonTalk),
+// ...[
+//   ,new Date(2024,8,10)
+// ].map(BMcDTalk),
+// ...[
+//   ,new Date(2024, 8, 11)
+// ].map(halliburtonTalk),
 // ...[
 //   , new Date(2024, 8, 11)
 // ].map(TSMC_ISSA),
