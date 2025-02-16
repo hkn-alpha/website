91bf8442fb5b35da8f637b121ddd6d49c3bdb91d
starblastbhan1 <sohumbhan@gmail.com>
2025-02-16 16:18:24 -0600

    ece 210

--- src/content/StudentServices/review_sessions.ts
+++ src/content/StudentServices/review_sessions.ts
@@ -61,6 +61,7 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]>= {
       time: "2/16, 12:30-3:00PM",
       // worksheetLink: "https://drive.google.com/file/d/1bB_NfQQvCMEPZxNIE9KnfAAhjhRjNV0J/view?usp=drive_link",
       slidesLink: "https://docs.google.com/presentation/d/1ntQbuGsQ_XgzD037_ANdnmT83o5SFXTLro_Yho12zng/edit?usp=sharing",
+      scribblesLink: "https://drive.google.com/drive/folders/1D6LsJFDzgv0g08M2F3IJo5VqSgJX3Q_m?usp=drive_link",
       // location: "ECEB 1013",
       // recordingLink:"https://mediaspace.illinois.edu/media/t/1_fgga5v5m"
     },
