af9eab2e6c9d33c0eed96fa2c10f0d232186a09f
Rithwik Rayani <39290434+therithwikrayani@users.noreply.github.com>
2025-11-07 21:31:02 -0600

    typo :(

--- src/content/StudentServices/review_sessions.ts
+++ src/content/StudentServices/review_sessions.ts
@@ -316,7 +316,7 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]>= {
       course: "ECE 340",
       time: "11/9, " + timeslots["B"],
       location: "ECEB 1015",
-      slidesLink: "https://docs.google.com/presentation/d/16KmkLnP2_I4o7z4mVP2_t_FsfuZVe1FY/edit?usp=sharing
+      slidesLink: "https://docs.google.com/presentation/d/16KmkLnP2_I4o7z4mVP2_t_FsfuZVe1FY/edit?usp=sharing",
       // slidesLink: "https://docs.google.com/presentation/d/1-sDLULISjV3wtzBqL1x4DznbLsd7v2cS0BKE6W3C7e4/edit?usp=sharing",
       // time: "11/10, 12:30-2:30PM",
       // location: "ECEB 1013",
