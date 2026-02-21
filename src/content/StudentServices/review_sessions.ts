edfea9af8975988914734cb6da026d71e134d7ad
SRC15 <156395465+SRC15@users.noreply.github.com>
2026-02-21 10:40:52 -0600

    Update review_sessions.ts
    220mt1ws

--- src/content/StudentServices/review_sessions.ts
+++ src/content/StudentServices/review_sessions.ts
@@ -71,11 +71,10 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]>= {
       //time: "TBD",
       location: "ECEB 1002",
       time: "2/22, " + timeslots["A"],
-      // location: "ECEB 1002",
-      //worksheetLink: "https://drive.google.com/file/d/1JkROts44OUQ7JnNYFdzAP4EZ1xlM7VwU/view?usp=sharing",
       //recordingLink: "https://mediaspace.illinois.edu/media/t/1_s3qqm0nc",
       slidesLink: "https://docs.google.com/presentation/d/1-xBhlkA78eSVJ9DylGd29rhBBBEWHAVc/edit?usp=drive_link&ouid=114909937515227017827&rtpof=true&sd=true",
-      //keyLink: "https://drive.google.com/file/d/1Q-27VqNKuLOtL_KHgistLxKPNC2VSiw_/view?usp=sharing"
+      worksheetLink: "https://drive.google.com/file/d/1X3r-DiWya_grxC-mAd0jOKzC9Kn8NV6Y/view?usp=sharing",
+      keyLink: "https://drive.google.com/file/d/1sbIVhEBfygCSzu51sBehhGUwBsnm8RfE/view?usp=sharing"
     },
     {
       course: "ECE 310",
