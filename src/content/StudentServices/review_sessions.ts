eb59553149e0cf6cccbe2b6d4b56be9bebb23b06
dchowe-2 <dchowe2@illinois.edu>
2026-09-20 18:03:35 -0500

    Update review_sessions.ts
    Add ECE329 and ECE110 videos

--- src/content/StudentServices/review_sessions.ts
+++ src/content/StudentServices/review_sessions.ts
@@ -47,7 +47,7 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]>= {
       // time: "TBD",
       // location: "TBD",
       slidesLink: "https://docs.google.com/presentation/d/1WTvsCjM3ZLmWROSjiXIKaMCjrihn2Jrp/edit?usp=sharing&ouid=110352225493747190043&rtpof=true&sd=true",
-      // recordingLink: "https://mediaspace.illinois.edu/media/t/1_r0svsi14",
+      recordingLink: "https://mediaspace.illinois.edu/media/t/1_z2a3ce09",
       scribblesLink: "https://drive.google.com/file/d/16_wEKtbcTdy56jtfMxgm8zw-3cJlQPe-/view?usp=sharing",
     },
     // {
@@ -121,7 +121,7 @@ export const reviewSessions: Record<1 | 2 | 3, ReviewSession[]>= {
       location: rooms[2],
       time: "9/20, " + timeslots["A"],
       // location: "ECEB 1013",
-      // recordingLink: "https://mediaspace.illinois.edu/media/t/1_at5usth0",
+      recordingLink: "https://mediaspace.illinois.edu/media/t/1_e89p58j3",
       slidesLink: "https://docs.google.com/presentation/d/1wg_pE8BB2IMmhAw37HS8dX8GF0R-AJyB/edit?usp=sharing&ouid=110352225493747190043&rtpof=true&sd=true",
       scribblesLink: "https://drive.google.com/file/d/1jktvH-_kWIYD-NCLGGV2VL34NvfPe58T/view?usp=sharing",
     },
